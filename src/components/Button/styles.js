8fcc9ce3e8590f13e6cbbb3013b277b2c3afb8db
ricardo <ricardo.br.pi@gmail.com>
2023-04-27 22:33:31 -0300

    feat: create button component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import colors from '../../tokens/colors'
+import screens from '../../tokens/screen.js'
+
+export const ButtonForm = styled.button`
+  background: ${colors.primary};
+  border: none;
+  color: white;
+  width: 190px;
+  height: 30px;
+  border-radius: 5px;
+
+  @media (min-width: ${screens.small}) {
+    width: 220px;
+  }
+`
